18689bb6e9253f57064f001f82d6f529878b47be
andrew-eldridge <eldridga@email.sc.edu>
2022-05-01 00:11:28 -0400

    CNN-LSTM network, command-line args, added % change feature

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import math
 import keras.callbacks
 import yfinance as yf
@@ -47,12 +48,26 @@ def denormalize(data, mean, std):
 
 
 def main():
+    # parse command-line args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-timesteps', default=10)
+    parser.add_argument('-num_epochs', default=100)
+    parser.add_argument('-learning_rate', default=0.0001)
+    args = parser.parse_args()
+
+    # model params
+    timesteps = int(args.timesteps)
+    num_features = 6
+    num_epochs = int(args.num_epochs)
+    learning_rate = float(args.learning_rate)
+    input_shape = (None, timesteps, num_features)
+
+    # fetch data
     data = yf.download('SPY', '1980-01-01', '2022-01-01')
-    # data = yf.download('BTC-USD', '2010-01-01', '2022-04-01')
-    # data2 = data2.drop(['Open', 'High', 'Low', 'Adj Close', 'Volume'], axis=1)
-    # data['Date'] = pd.to_datetime(data['Date'], format='%Y-%m-%d')
-    # data.set_index('Date', inplace=True)
     data = data.drop(['Adj Close'], axis=1)
+    percent_change = [(data['Close'][i] - data['Close'][i-1]) / data['Close'][i-1] for i in range(1, data.shape[0])]
+    data = data[1:]
+    data['Change'] = percent_change
     print(data.head(10))
 
     # split into train/val/test sets
@@ -64,40 +79,32 @@ def main():
     test_data = data[cutoff2:]
 
     # normalize data
-    X_train, y_train, _, _ = get_X_y(train_data)
-    X_val, y_val, _, _ = get_X_y(val_data)
-    X_test, y_test, test_mean, test_std = get_X_y(test_data)
+    X_train, y_train, _, _ = get_X_y(train_data, timesteps)
+    X_val, y_val, _, _ = get_X_y(val_data, timesteps)
+    X_test, y_test, test_mean, test_std = get_X_y(test_data, timesteps)
 
     # build CNN-LSTM network
     model = Sequential()
-    model.add(InputLayer((10, 5)))
-    # model.add(Conv1D(64, kernel_size=2, activation='relu'))
-    # model.add(Flatten())
+    model.add(Conv1D(128, 3, activation='relu', input_shape=(timesteps, num_features)))
+    model.add(MaxPooling1D(pool_size=2))
+    model.add(Flatten())
+    model.add(RepeatVector(1))
     model.add(LSTM(64, activation='softsign'))
-    #model.add(Dense(8, 'relu'))
-    #model.add(LSTM(5, activation='softsign'))
+    model.add(Flatten())
     model.add(Dense(1, 'linear'))
+    model.build(input_shape=input_shape)
     print(model.summary())
-
-    # model = tf.keras.Sequential([
-    #     InputLayer((10, 1)),
-    #     LSTM(5, return_sequences=True, activation='softsign'),
-    #     Dropout(0.10),
-    #     LSTM(50, return_sequences=True, activation='softsign'),
-    #     Dropout(0.10),
-    #     LSTM(20),
-    #     Dense(1, 'linear')
-    # ])
-    # model.build()
-    # print(model.summary())
 
     cp = keras.callbacks.ModelCheckpoint('model/',
                                          save_best_only=True)
-    model.compile(loss=tf.keras.losses.MeanSquaredError(), optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
-                   metrics=[RootMeanSquaredError()])
-    model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=100, callbacks=[cp])
+    model.compile(loss=tf.keras.losses.MeanSquaredError(), optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
+                  metrics=[RootMeanSquaredError()])
+    model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=num_epochs, callbacks=[cp])
 
     # y_pred = denormalize(pd.DataFrame(model.predict(X).flatten()), mean, std).to_numpy()
+    mse, rmse = model.evaluate(X_test, y_test)
+    print(f'Test loss (MSE): {mse}')
+    print(f'Test RMSE: {rmse}')
     y_pred = denormalize(pd.DataFrame(model.predict(X_test).flatten()), test_mean, test_std).to_numpy()
     y_test = denormalize(pd.DataFrame(y_test), test_mean, test_std).to_numpy()
     plot_predicted_vs_true(y_pred, y_test)
